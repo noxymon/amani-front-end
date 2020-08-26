66a072596e7c12a59c0c4235aaa599d7ec0a1dfd
noxymon <avicienna.ulhaq@gmail.com>
2020-08-26 22:58:08 +0700

    Fix bugs registration user

--- src/main/java/id/akademi/amanifo/registration/services/models/MemberRegisterBackendRequest.java
+++ src/main/java/id/akademi/amanifo/registration/services/models/MemberRegisterBackendRequest.java
@@ -1,6 +1,8 @@
 package id.akademi.amanifo.registration.services.models;
 
 import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import id.akademi.amanifo.registration.utils.MemberType;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +15,7 @@ public class MemberRegisterBackendRequest
     private String    lastName;
     private String    password;
     private String    firstName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
     @Builder.Default
     private String    memberType = MemberType.REGULER.getMemberTypeId();
