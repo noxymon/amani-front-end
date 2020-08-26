b20930756062c39258c60e4655c970107ad0c369
noxymon <avicienna.ulhaq@gmail.com>
2020-08-26 23:52:50 +0700

    Fix image, detail & floor, using internal image

--- src/main/java/id/akademi/amanifo/course/services/models/CourseResult.java
+++ src/main/java/id/akademi/amanifo/course/services/models/CourseResult.java
@@ -30,31 +30,33 @@ public class CourseResult
     private String    courseOutline;
     private boolean   alreadyJoined;
     private boolean   openForRegistration;
+    private String    floorImage;
+    private String    courseDetailImage;
 
-    public CourseResult(CourseResult existing)
+    public CourseResult(CourseResult other)
     {
-        this.id = existing.id;
-        this.courseName =  existing.courseName;
-        this.courseDescription =  existing.courseDescription;
-        this.courseStartDate = existing.courseEndDate;
-        this.courseEndDate = existing.courseEndDate;
-        this.courseStartTime = existing.courseStartTime;
-        this.courseEndTime = existing.courseEndTime;
-        this.published = existing.published;
-        this.courseType =  existing.courseType;
-        this.instructorName =  existing.instructorName;
-        this.instructorDescription =  existing.instructorDescription;
-        this.webinarLink =  existing.webinarLink;
-        this.latitude = existing.latitude;
-        this.longitude = existing.longitude;
-        this.capacity = existing.capacity;
-        this.registeredCount =  existing.registeredCount;
-        this.daysBeforeStartDate = existing.daysBeforeStartDate;
-        this.meetingId =  existing.meetingId;
-        this.courseOutline =  existing.courseOutline;
-        this.alreadyJoined =  existing.alreadyJoined;
-        this.openForRegistration = existing.openForRegistration;
+        this.id = other.id;
+        this.courseName = other.courseName;
+        this.courseDescription = other.courseDescription;
+        this.courseStartDate = other.courseStartDate;
+        this.courseEndDate = other.courseEndDate;
+        this.courseStartTime = other.courseStartTime;
+        this.courseEndTime = other.courseEndTime;
+        this.published = other.published;
+        this.courseType = other.courseType;
+        this.instructorName = other.instructorName;
+        this.instructorDescription = other.instructorDescription;
+        this.webinarLink = other.webinarLink;
+        this.latitude = other.latitude;
+        this.longitude = other.longitude;
+        this.capacity = other.capacity;
+        this.registeredCount = other.registeredCount;
+        this.daysBeforeStartDate = other.daysBeforeStartDate;
+        this.meetingId = other.meetingId;
+        this.courseOutline = other.courseOutline;
+        this.alreadyJoined = other.alreadyJoined;
+        this.openForRegistration = other.openForRegistration;
+        this.floorImage = other.floorImage;
+        this.courseDetailImage = other.courseDetailImage;
     }
-
-    
 }
--- src/main/java/id/akademi/amanifo/course/services/models/CourseResultList.java
+++ src/main/java/id/akademi/amanifo/course/services/models/CourseResultList.java
@@ -1,10 +1,14 @@
 package id.akademi.amanifo.course.services.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseResultList
 {
     private List<CourseResult> courseResponseList;
